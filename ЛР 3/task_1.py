95efdd60141cb552876576e6ac33ce271f0fc906
shuryashkina-eo <149330908+shuryashkina-eo@users.noreply.github.com>
2023-11-15 23:32:41 +0300

    Update task_1.py

--- ЛР 3/task_1.py
+++ ЛР 3/task_1.py
@@ -2,14 +2,19 @@
 
 
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
-
 for find_item in ['банан', 'груша', 'персик']:
-    index_item = {items_list }
-
-    for i in index_item:
-        if items_list (i) == find_item:
+    condition = 0
+    for i in items_list:
+        if i == find_item:
+            index_item = items_list.index(i)
+            print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+            break
+        else:
+            condition += 1
+            if items_list.index(i) == items_list.index(items_list[-1]):
+             if condition == len(items_list):
+              print(f"Товар '{find_item}' не найден в списке.")
     # TODO Вызовите функцию, что получить индекс товара
-         if index_item is not None:
-             print(f"Первое вхождение товара '{find_item}' имеет индекс {i}.")
-         else: vvvv
-             print(f"Товар '{find_item}' не найден в списке.")
+
+
+
